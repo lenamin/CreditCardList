bdc045705d27111ec6bf7363c38057cd5eb42214
Lena <99120199+lenamin@users.noreply.github.com>
2022-06-09 20:39:18 +0900

    lottie animation 구현

--- CreditCardList/CardDetailViewController.swift
+++ CreditCardList/CardDetailViewController.swift
@@ -11,4 +11,15 @@ import Lottie
 class CardDetailViewController: UIViewController {
     
     @IBOutlet weak var lottieView: AnimationView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let animationView = AnimationView(name: "money") // 1. animationView 선언 및 가져올 json 파일 이름
+        lottieView.contentMode = .scaleToFill
+        lottieView.addSubview(animationView)
+        animationView.frame = lottieView.bounds
+        animationView.loopMode = .loop // 반복 설정
+        animationView.play()
+    }
 }
