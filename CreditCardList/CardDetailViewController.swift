59fdab0523a83920ea1a8081c9095ea6686fe0c1
Lena <99120199+lenamin@users.noreply.github.com>
2022-06-09 20:14:26 +0900

    [UI] CardDetailView UI 그리기

---
+++ CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by Lena on 2022/06/09.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+    
+}
