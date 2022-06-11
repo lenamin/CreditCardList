3307d792f7d2f17979938021265afce25adc316c
Lena <99120199+lenamin@users.noreply.github.com>
2022-06-11 22:58:09 +0900

    #1 Firebase realtime database 데이터 읽기

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -6,8 +6,10 @@
 //
 import UIKit
 import Kingfisher
+import FirebaseDatabase
 
 class CardListViewController: UITableViewController {
+    var ref: DatabaseReference! // Firebase Realtime Database
     var creditCardList: [CreditCard] = [] // 전달된 데이터
     
     override func viewDidLoad() {
@@ -17,6 +19,31 @@ class CardListViewController: UITableViewController {
         let nibName = UINib(nibName: "CardListCell", bundle: nil) // nib 이름
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
         // 이렇게 생성한 nib name을 가지는 것에 register를 한다
+        ref = Database.database().reference()
+        // firebase가 우리의 database를 잡아내고 앞으로의 데이터 흐름들을 주고받는다
+        
+        ref.observe(.value) { snapshot in
+            // snapshot 이용해서 데이터를 불러온다
+            // 레퍼런스에서 값을 지켜보고 있다가 객체를 이용한다.
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                
+                // 이렇게 가져온 카드리스트를 미리 만들어놓은 creditcard list에 넣어준다
+                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
+                
+                // 테이블뷰 리로드 해줄 것임 (UI의 움직임)
+                // 메인스레드에서 작동해야 하므로 dispatchQueue를 해준 것 
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                } // 테이블뷰 업데이트
+                
+            } catch let error {
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        }
     }
     
     // 데이터를 셀에 전달한다
